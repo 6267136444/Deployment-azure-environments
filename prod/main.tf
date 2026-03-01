556acedeea743c23b4643f5fb470cbe607bcad0b
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-01 12:19:25 +0530

    Create main.tf

---
+++ prod/main.tf
@@ -0,0 +1,9 @@
+module "linux_vm" {
+  source = "git::https://github.com/YOUR-USERNAME/terraform-azure-modules.git//modules/linux-vm?ref=main"
+
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  vm_name             = var.vm_name
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+}
