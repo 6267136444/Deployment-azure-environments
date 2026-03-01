5876eafab5aa1c765d715014abef266f54e76f13
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-01 12:17:04 +0530

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,3 +1,25 @@
-provider "azurerm" {
-  features {}
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Virtual machine name"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for VM"
+  type        = string
+  sensitive   = true
 }
