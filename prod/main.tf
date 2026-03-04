5541e84540100d7ba77e286d075b325a9096e8e8
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-04 22:57:28 +0530

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -1,5 +1,5 @@
 module "linux_vm" {
-  source = "git::https://github.com/YOUR-USERNAME/terraform-azure-modules.git//modules/linux-vm?ref=main"
+  source = "git::https://github.com/6267136444/terraform-azure-modules.git//modules/linux-vm?ref=main"
 
   location            = var.location
   resource_group_name = var.resource_group_name
