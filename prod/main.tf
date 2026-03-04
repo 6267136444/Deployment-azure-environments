f4d365b03eb495110dd7d6cab0331d646c7e08f6
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 00:02:14 +0530

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -4,6 +4,7 @@ module "linux_vm" {
   location            = var.location
   resource_group_name = var.resource_group_name
   vm_name             = var.vm_name
+  vm_size             = var.vm_size
   admin_username      = var.admin_username
   admin_password      = var.admin_password
 }
