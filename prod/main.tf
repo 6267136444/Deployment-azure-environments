556d48f379a782b234d5c292e1883b1a0a59b5dc
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 18:32:10 +0530

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -24,6 +24,13 @@ resource "azurerm_subnet" "db_subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "web_pip" {
+  name                = "webserver-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
 resource "azurerm_network_interface" "web_nic" {
   name                = "web-nic"
   location            = azurerm_resource_group.rg.location
