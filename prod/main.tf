107a88c32b0faec2c717354a96ea8d4fb7a38b40
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 11:58:55 +0530

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -1,10 +1,21 @@
-module "linux_vm" {
+module "web_server" {
   source = "git::https://github.com/6267136444/terraform-azure-modules.git//modules/linux-vm?ref=main"
 
   location            = var.location
   resource_group_name = var.resource_group_name
-  vm_name             = var.vm_name
-  vm_size             = var.vm_size
+  vm_name             = "webserver"
+  vm_size             = "Standard_D2as_v5"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+}
+
+module "db_server" {
+  source = "git::https://github.com/6267136444/terraform-azure-modules.git//modules/linux-vm?ref=main"
+
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  vm_name             = "dbserver"
+  vm_size             = "Standard_D4als_v6"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
 }
