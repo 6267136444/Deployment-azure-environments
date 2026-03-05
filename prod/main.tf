c5139d21bf7390d56d83e991e7b49e259602f140
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 17:01:34 +0530

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -57,7 +57,7 @@ resource "azurerm_network_interface" "db_nic" {
 }
 
 module "web_server" {
-  source = "./modules/linux-vm"
+  source = "git::https://github.com/6267136444/terraform-azure-modules.git//modules/linux-vm"
 
   vm_name             = "server-web"
   location            = var.location
@@ -71,7 +71,7 @@ module "web_server" {
 }
 
 module "db_server" {
-  source = "./modules/linux-vm"
+  source = "git::https://github.com/6267136444/terraform-azure-modules.git//modules/linux-vm"
 
   vm_name             = "server-db"
   location            = var.location
